3369ce29cea5d6a5194fa93a80fbc33e74b4dc35
dankuri <danek.il420@gmail.com>
2023-08-15 22:49:54 +0200

    some formatting

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,8 +1,18 @@
 local configs = require("nvim-treesitter.configs")
 
 configs.setup({
-	ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "go", "rust", "html", "css" },
-	sync_install = false,
-	highlight = { enable = true },
-	indent = { enable = true },
+    ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "javascript",
+        "typescript",
+        "go",
+        "rust",
+        "html",
+        "css"
+    },
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
 })
