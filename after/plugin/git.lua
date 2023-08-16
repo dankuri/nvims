1496e67a246627dea699dd8dbe46cc854f0fcf8b
dankuri <danek.il420@gmail.com>
2023-08-16 17:38:48 +0200

    mooore keymaps

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -2,7 +2,12 @@ require("gitsigns").setup({
 	on_attach = function(bufnr)
 		vim.keymap.set("n", "[c", require("gitsigns").prev_hunk, { buffer = bufnr, desc = "prev hunk" })
 		vim.keymap.set("n", "]c", require("gitsigns").next_hunk, { buffer = bufnr, desc = "next hunk" })
-		vim.keymap.set("n", "gp", require("gitsigns").preview_hunk_inline, { buffer = bufnr, desc = "preview hunk" })
-		vim.keymap.set("n", "bl", require("gitsigns").blame_line, { buffer = bufnr, desc = "blame line" })
+		vim.keymap.set(
+			"n",
+			"<leader>gp",
+			require("gitsigns").preview_hunk_inline,
+			{ buffer = bufnr, desc = "preview hunk" }
+		)
+		vim.keymap.set("n", "<leader>gb", require("gitsigns").blame_line, { buffer = bufnr, desc = "blame line" })
 	end,
 })
