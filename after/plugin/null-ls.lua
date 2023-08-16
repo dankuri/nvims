7b1464ca3e5e7b3d584695c9479776fa0ba8860c
dankuri <danek.il420@gmail.com>
2023-08-16 15:19:39 +0200

    add null-ls

---
+++ after/plugin/null-ls.lua
@@ -0,0 +1,53 @@
+local ok, null_ls = pcall(require, "null-ls")
+
+if not ok then
+	return
+end
+
+local sources = {
+	null_ls.builtins.formatting.rustfmt,
+	null_ls.builtins.formatting.prettierd.with({
+		filetypes = { "html", "json", "markdown", "scss", "css", "typescript", "javascript", "vue", "yaml" },
+	}),
+	-- null_ls.builtins.diagnostics.eslint.with {
+	--   command = "eslint_d",
+	-- },
+	-- null_ls.builtins.formatting.shfmt,
+	null_ls.builtins.diagnostics.shellcheck.with({
+		diagnostics_format = "#{m} [#{c}]",
+	}),
+	null_ls.builtins.formatting.stylua,
+	null_ls.builtins.formatting.gofumpt,
+	null_ls.builtins.formatting.goimports_reviser,
+	null_ls.builtins.formatting.golines,
+	-- null_ls.builtins.diagnostics.revive,
+	-- null_ls.builtins.diagnostics.golangci_lint
+}
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local on_attach = function(client, bufnr)
+	local format_is_enabled = true
+	vim.api.nvim_create_user_command("ToggleFormat", function()
+		format_is_enabled = not format_is_enabled
+		print("Setting autoformatting to: " .. tostring(format_is_enabled))
+	end, {})
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				if not format_is_enabled then
+					return
+				end
+				vim.lsp.buf.format({ bufnr = bufnr })
+			end,
+		})
+	end
+end
+
+null_ls.setup({
+	sources = sources,
+	on_attach = on_attach,
+	debug = false,
+})
--- lua/dankuri/lazy.lua
+++ lua/dankuri/lazy.lua
@@ -1,119 +1,120 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    {
-        'Mofiqul/dracula.nvim'
-    },
-    {
-        'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-    },
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        dependencies = {
-            -- LSP Support
-            {
-                'neovim/nvim-lspconfig',
-                dependencies = {
-                    -- Useful status updates for LSP
-                    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-                    { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-                }
-            },                                       -- Required
-            { 'williamboman/mason.nvim' },           -- Optional
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+	{
+		"Mofiqul/dracula.nvim",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		dependencies = {
+			-- LSP Support
+			{
+				"neovim/nvim-lspconfig",
+				dependencies = {
+					-- Useful status updates for LSP
+					-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+					{ "j-hui/fidget.nvim", tag = "legacy", opts = {} },
+					{ "jose-elias-alvarez/null-ls.nvim" }, -- WARN: it is archived, check https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1621 if it breaks
+				},
+			}, -- Required
+			{ "williamboman/mason.nvim" }, -- Optional
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' },     -- Required
-        }
-    },
-    { 'folke/which-key.nvim',  opts = {} },
-    {
-        -- Set lualine as statusline
-        'nvim-lualine/lualine.nvim',
-        -- See `:help lualine.txt`
-        opts = {
-            options = {
-                icons_enabled = false,
-                theme = 'dracula',
-                component_separators = '|',
-                section_separators = '',
-            },
-        },
-    },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
+		},
+	},
+	{ "folke/which-key.nvim", opts = {} },
+	{
+		-- Set lualine as statusline
+		"nvim-lualine/lualine.nvim",
+		-- See `:help lualine.txt`
+		opts = {
+			options = {
+				icons_enabled = false,
+				theme = "dracula",
+				component_separators = "|",
+				section_separators = "",
+			},
+		},
+	},
 
-    {
-        -- Add indentation guides even on blank lines
-        'lukas-reineke/indent-blankline.nvim',
-        -- Enable `lukas-reineke/indent-blankline.nvim`
-        -- See `:help indent_blankline.txt`
-        opts = {
-            char_highlight_list = {
-                "IndentBlanklineIndent1",
-                "IndentBlanklineIndent2",
-                "IndentBlanklineIndent3",
-            },
-            show_trailing_blankline_indent = false,
-            show_current_context = true,
-            show_current_context_start = true,
-        },
-    },
+	{
+		-- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help indent_blankline.txt`
+		opts = {
+			char_highlight_list = {
+				"IndentBlanklineIndent1",
+				"IndentBlanklineIndent2",
+				"IndentBlanklineIndent3",
+			},
+			show_trailing_blankline_indent = false,
+			show_current_context = true,
+			show_current_context_start = true,
+		},
+	},
 
-    -- "gc" to comment visual regions/lines
-    { 'numToStr/Comment.nvim', opts = {} },
-    {
-        'lewis6991/gitsigns.nvim',
-        event = 'BufRead',
-    },
-    {
-        "kdheepak/lazygit.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-    },
-    {
-        "nvim-treesitter/nvim-treesitter-context",
-        event = "BufEnter",
-    },
-    {
-        "christoomey/vim-tmux-navigator",
-        lazy = false,
-    },
-    {
-        'ThePrimeagen/harpoon',
-        lazy = false,
-    },
-    {
-        'mbbill/undotree',
-        event = "BufRead",
-    },
-    {
-        "kylechui/nvim-surround",
-        version = "*", -- Use for stability; omit to use `main` branch for the latest features
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup {
-                -- Configuration here, or leave empty to use defaults
-            }
-        end,
-    },
-    -- Detect tabstop and shiftwidth automatically
-    'tpope/vim-sleuth',
+	-- "gc" to comment visual regions/lines
+	{ "numToStr/Comment.nvim", opts = {} },
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "BufRead",
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		event = "BufEnter",
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{
+		"ThePrimeagen/harpoon",
+		lazy = false,
+	},
+	{
+		"mbbill/undotree",
+		event = "BufRead",
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	-- Detect tabstop and shiftwidth automatically
+	"tpope/vim-sleuth",
 })
