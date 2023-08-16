482a0a692c8a55b25172777dbda93ac67a335364
dankuri <danek.il420@gmail.com>
2023-08-16 14:35:35 +0200

    add some plugins from kickstart.nvim

--- lua/dankuri/lazy.lua
+++ lua/dankuri/lazy.lua
@@ -29,7 +29,14 @@ require("lazy").setup({
         branch = 'v2.x',
         dependencies = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' },             -- Required
+            {
+                'neovim/nvim-lspconfig',
+                dependencies = {
+                    -- Useful status updates for LSP
+                    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+                    { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+                }
+            },                                       -- Required
             { 'williamboman/mason.nvim' },           -- Optional
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
@@ -39,7 +46,40 @@ require("lazy").setup({
             { 'L3MON4D3/LuaSnip' },     -- Required
         }
     },
-    { 'folke/which-key.nvim', opts = {} },
+    { 'folke/which-key.nvim',  opts = {} },
+    {
+        -- Set lualine as statusline
+        'nvim-lualine/lualine.nvim',
+        -- See `:help lualine.txt`
+        opts = {
+            options = {
+                icons_enabled = false,
+                theme = 'dracula',
+                component_separators = '|',
+                section_separators = '',
+            },
+        },
+    },
+
+    {
+        -- Add indentation guides even on blank lines
+        'lukas-reineke/indent-blankline.nvim',
+        -- Enable `lukas-reineke/indent-blankline.nvim`
+        -- See `:help indent_blankline.txt`
+        opts = {
+            char_highlight_list = {
+                "IndentBlanklineIndent1",
+                "IndentBlanklineIndent2",
+                "IndentBlanklineIndent3",
+            },
+            show_trailing_blankline_indent = false,
+            show_current_context = true,
+            show_current_context_start = true,
+        },
+    },
+
+    -- "gc" to comment visual regions/lines
+    { 'numToStr/Comment.nvim', opts = {} },
     {
         'lewis6991/gitsigns.nvim',
         event = 'BufRead',
@@ -74,4 +114,6 @@ require("lazy").setup({
             }
         end,
     },
+    -- Detect tabstop and shiftwidth automatically
+    'tpope/vim-sleuth',
 })
