fc57f0a53114f0b342867495c7bc9715dc1df58e
dankuri <danek.il420@gmail.com>
2023-09-03 20:09:02 +0200

    uncomment leader x keybinding

--- lua/dankuri/remap.lua
+++ lua/dankuri/remap.lua
@@ -30,7 +30,7 @@ vim.keymap.set("n", "Q", "<nop>")
 -- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "substitute word" })
--- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "make file executable" })
 
 -- vim.keymap.set("n", "<leader><leader>", function()
 --   vim.cmd("so")
