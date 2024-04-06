10be5ad79a78656633eea0e52ce77172e0f842c1
dankuri <danek.il420@gmail.com>
2024-04-06 13:56:47 +0200

    add yaml-companion, im kubering

--- lua/dankuri/lazy.lua
+++ lua/dankuri/lazy.lua
@@ -112,6 +112,25 @@ require("lazy").setup({
 			})
 		end,
 	},
+	{
+		"someone-stole-my-name/yaml-companion.nvim",
+		ft = { "yaml" },
+		dependencies = {
+			{ "neovim/nvim-lspconfig" },
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope.nvim" },
+		},
+		opts = {
+			builtin_matchers = {
+				kubernetes = { enabled = true },
+			},
+		},
+		config = function(_, opts)
+			local cfg = require("yaml-companion").setup(opts)
+			require("lspconfig")["yamlls"].setup(cfg)
+			require("telescope").load_extension("yaml_schema")
+		end,
+	},
 	{ "folke/which-key.nvim", opts = {} },
 	{
 		-- Set lualine as statusline
