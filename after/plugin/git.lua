be860a5321f3b605bc023f5359fbdc8d6adf4cf4
dankuri <danek.il420@gmail.com>
2024-05-28 16:05:52 +0200

    update git keymaps & plugins

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -1,13 +1,14 @@
 require("gitsigns").setup({
 	on_attach = function(bufnr)
-		vim.keymap.set("n", "[c", require("gitsigns").prev_hunk, { buffer = bufnr, desc = "prev hunk" })
-		vim.keymap.set("n", "]c", require("gitsigns").next_hunk, { buffer = bufnr, desc = "next hunk" })
+		vim.keymap.set("n", "[g", require("gitsigns").prev_hunk, { buffer = bufnr, desc = "prev git hunk" })
+		vim.keymap.set("n", "]g", require("gitsigns").next_hunk, { buffer = bufnr, desc = "next git hunk" })
 		vim.keymap.set(
 			"n",
 			"<leader>gp",
 			require("gitsigns").preview_hunk_inline,
 			{ buffer = bufnr, desc = "preview hunk" }
 		)
+		vim.keymap.set("n", "<leader>gr", require("gitsigns").reset_hunk, { buffer = bufnr, desc = "reset hunk" })
 		vim.keymap.set("n", "<leader>gb", require("gitsigns").blame_line, { buffer = bufnr, desc = "blame line" })
 	end,
 })
