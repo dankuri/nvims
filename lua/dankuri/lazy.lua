88a6e1b4d95e9004d196d759ed3a07c777be0a5a
dankuri <danek.il420@gmail.com>
2024-07-17 15:56:23 +0200

    add nvim-dap & go debugger config

--- lua/dankuri/lazy.lua
+++ lua/dankuri/lazy.lua
@@ -73,6 +73,42 @@ require("lazy").setup({
 		},
 	},
 	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"leoluz/nvim-dap-go",
+			{
+				"rcarriga/nvim-dap-ui",
+				dependencies = {
+					"nvim-neotest/nvim-nio",
+				},
+			},
+		},
+		config = function()
+			require("dapui").setup()
+			require("dap-go").setup()
+
+			local dap, dapui = require("dap"), require("dapui")
+
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+
+			vim.keymap.set("n", "<Leader>db", ":DapToggleBreakpoint<CR>", { desc = "DAP: toggle breakpoing" })
+			vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>", { desc = "DAP: start or continue" })
+			vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>", { desc = "DAP: terminate" })
+			vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>", { desc = "DAP: step over" })
+		end,
+	},
+	{
 		"olexsmir/gopher.nvim",
 		ft = "go",
 		config = function()
