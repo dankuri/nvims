ffe5dea1a2bce47399474d9619bfa966ec331f42
dankuri <danek.il420@gmail.com>
2024-07-17 16:09:06 +0200

    add the best select ui plugin (telescope)

--- lua/dankuri/lazy.lua
+++ lua/dankuri/lazy.lua
@@ -16,9 +16,23 @@ require("lazy").setup({
 		"Mofiqul/dracula.nvim",
 	},
 	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
 		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local telescope = require("telescope")
+			telescope.setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			telescope.load_extension("ui-select")
+		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
