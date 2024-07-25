1fa8aceb537b1727f83ebe0e374f3855f5bbfc4b
dankuri <danek.il420@gmail.com>
2024-07-25 16:48:29 +0200

    add better completion in cmdline

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -51,3 +51,26 @@ cmp.setup.filetype({ "sql" }, {
 		{ name = "buffer" },
 	},
 })
+
+-- `/` cmdline setup.
+cmp.setup.cmdline("/", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
+})
+
+-- `:` cmdline setup.
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{
+			name = "cmdline",
+			option = {
+				ignore_cmds = { "Man", "!" },
+			},
+		},
+	}),
+})
--- lua/dankuri/lazy.lua
+++ lua/dankuri/lazy.lua
@@ -80,6 +80,7 @@ require("lazy").setup({
 			{ "hrsh7th/cmp-nvim-lua" },
 			{ "hrsh7th/cmp-path" },
 			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-cmdline" },
 
 			{ "L3MON4D3/LuaSnip" },
 			{ "saadparwaiz1/cmp_luasnip" },
