5f21a4c0ea4f12c5d9fcd5ec1a8623fe8b7c6e72
dankuri <danek.il420@gmail.com>
2024-11-08 15:28:07 +0100

    fix fzf-native not loading

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,12 +1,12 @@
 return {
 	{
-		"nvim-telescope/telescope-ui-select.nvim",
-	},
-	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
 		config = function()
 			local telescope = require("telescope")
 			telescope.setup({
