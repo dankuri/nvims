2900807d51a579174dc180aee6c6edb73b188851
dankuri <danek.il420@gmail.com>
2024-12-03 18:07:34 +0500

    macos trash is trash

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -164,20 +164,26 @@ require("lazy").setup({
 	},
 	{
 		"stevearc/oil.nvim",
-		opts = {
-			delete_to_trash = true,
-			skip_confirm_for_simple_edits = true,
-			win_options = {
-				signcolumn = "yes:2",
-			},
-			view_options = {
-				show_hidden = true,
-				natural_order = true,
-				is_always_hidden = function(name, _)
-					return name == ".."
-				end,
-			},
-		},
+		config = function()
+			require("oil").setup({
+				delete_to_trash = true,
+				skip_confirm_for_simple_edits = true,
+				win_options = {
+					signcolumn = "yes:2",
+				},
+				view_options = {
+					show_hidden = true,
+					natural_order = true,
+					is_always_hidden = function(name, _)
+						return name == ".."
+					end,
+				},
+			})
+			-- use trash-cli instead of macos builtin trash
+			if vim.loop.os_uname().sysname == "Darwin" and vim.fn.exepath("trash") ~= "" then
+				package.loaded["oil.adapters.trash"] = require("oil.adapters.trash.freedesktop")
+			end
+		end,
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
 			opts = {
