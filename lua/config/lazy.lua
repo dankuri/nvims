5ecddced2ec05343e271f95c13831675165bae43
dankuri <danek.il420@gmail.com>
2025-02-20 21:40:14 +0500

    add YankAssassin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -129,6 +129,17 @@ require("lazy").setup({
 		end,
 	},
 	{
+		"svban/YankAssassin.nvim",
+		config = function()
+			require("YankAssassin").setup({
+				auto_normal = true, -- if true, autocmds are used. Whenever y is used in normal mode, the cursor doesn't move to start
+				auto_visual = true, -- if true, autocmds are used. Whenever y is used in visual mode, the cursor doesn't move to start
+			})
+			vim.keymap.set({ "x", "n" }, "gy", "<Plug>(YADefault)", { silent = true })
+			-- vim.keymap.set({ "x", "n" }, "<leader>y", "<Plug>(YANoMove)", { silent = true })
+		end,
+	},
+	{
 		"OXY2DEV/markview.nvim",
 		lazy = false,
 		init = function()
