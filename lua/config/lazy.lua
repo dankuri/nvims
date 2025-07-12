d6ff96c27f2942144267c2c2a2c66923086b598d
dankuri <danek.il420@gmail.com>
2025-07-13 00:35:08 +0500

    add render-markdown.nvim (replace markview)

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -77,6 +77,15 @@ require("lazy").setup({
 		},
 	},
 	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {
+			completions = { lsp = { enabled = true } },
+		},
+	},
+	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
 		opts = {
