542d0e6db3153109e420c9448522a867b15506a3
dankuri <danek.il420@gmail.com>
2025-09-19 03:03:38 +0300

    add tpope/fugitive

--- lua/config/plugins/git.lua
+++ lua/config/plugins/git.lua
@@ -1,36 +1,50 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	event = "BufRead",
-	config = function()
-		require("gitsigns").setup({
-			numhl = true,
-			on_attach = function(bufnr)
-				vim.keymap.set("n", "[g", function()
-					require("gitsigns").nav_hunk("prev")
-				end, { buffer = bufnr, desc = "prev git hunk" })
-				vim.keymap.set("n", "]g", function()
-					require("gitsigns").nav_hunk("next")
-				end, { buffer = bufnr, desc = "next git hunk" })
-				vim.keymap.set(
-					"n",
-					"<leader>gp",
-					require("gitsigns").preview_hunk_inline,
-					{ buffer = bufnr, desc = "preview hunk" }
-				)
-				vim.keymap.set(
-					"n",
-					"<leader>gR",
-					require("gitsigns").reset_hunk,
-					{ buffer = bufnr, desc = "reset hunk" }
-				)
-				vim.keymap.set(
-					"n",
-					"<leader>gb",
-					require("gitsigns").blame_line,
-					{ buffer = bufnr, desc = "blame line" }
-				)
-				vim.keymap.set("n", "<leader>gB", require("gitsigns").blame, { buffer = bufnr, desc = "blame side" })
-			end,
-		})
-	end,
+	{
+		"tpope/vim-fugitive",
+		dependencies = {
+			"tpope/vim-rhubarb",
+			"shumphrey/fugitive-gitlab.vim",
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "BufRead",
+		config = function()
+			require("gitsigns").setup({
+				numhl = true,
+				on_attach = function(bufnr)
+					vim.keymap.set("n", "[g", function()
+						require("gitsigns").nav_hunk("prev")
+					end, { buffer = bufnr, desc = "prev git hunk" })
+					vim.keymap.set("n", "]g", function()
+						require("gitsigns").nav_hunk("next")
+					end, { buffer = bufnr, desc = "next git hunk" })
+					vim.keymap.set(
+						"n",
+						"<leader>gp",
+						require("gitsigns").preview_hunk_inline,
+						{ buffer = bufnr, desc = "preview hunk" }
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>gR",
+						require("gitsigns").reset_hunk,
+						{ buffer = bufnr, desc = "reset hunk" }
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>gb",
+						require("gitsigns").blame_line,
+						{ buffer = bufnr, desc = "blame line" }
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>gB",
+						require("gitsigns").blame,
+						{ buffer = bufnr, desc = "blame side" }
+					)
+				end,
+			})
+		end,
+	},
 }
