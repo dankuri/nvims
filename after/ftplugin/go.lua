fd0a16704e80b07ba9aa157ac847f33e6e2d019d
dankuri <danek.il420@gmail.com>
2026-02-02 18:36:16 +0500

    use ,e for GoIfErr

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("n", "<localleader>ie", ":GoIfErr<CR>", { silent = true, desc = "GO: if err" })
+vim.keymap.set("n", "<localleader>e", ":GoIfErr<CR>", { silent = true, desc = "GO: if err" })
 vim.keymap.set("n", "<localleader>g", ":GoGenerate<CR>", { silent = true, desc = "GO: generate" })
 vim.keymap.set("n", "<localleader>at", ":GoAddTag ", { desc = "GO: add tag" })
