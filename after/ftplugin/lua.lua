f77ae5b71ed78aac4b1a60f22a499a6c421317b6
dankuri <danek.il420@gmail.com>
2026-05-12 11:54:28 +0500

    disable lazydev when .luarc.json is present

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -3,4 +3,5 @@ require("lazydev").setup({
 	library = {
 		{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 	},
+	enabled = function(root_dir) return not vim.uv.fs_stat(root_dir .. "/.luarc.json") end,
 })
